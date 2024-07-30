92c571ac50c6c2d1148b29eb793525edbe15c490
Nick Burt <nbur4556@localhost.localdomain>
2024-07-30 16:18:38 -0500

    fix plenary dependency path for todo comments

--- lua/plugins/todocomments.lua
+++ lua/plugins/todocomments.lua
@@ -1,6 +1,6 @@
 -- Comment highlighting for specific comment types (TODO, NOTE, HACK, FIX)
 return {
 	'folke/todo-comments.nvim',
-	dependencies = { 'nvim-lua//plenary.nvim' },
+	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = {}
 };
