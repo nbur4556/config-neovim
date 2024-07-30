07299ff2d285f76b203aa272c5378ba206d5ba43
Nick Burt <nbur4556@localhost.localdomain>
2024-07-30 16:41:04 -0500

    use new spec for whichkey keybindings

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,21 +1,19 @@
 -- displays command and key chain options and help
 return { 'folke/which-key.nvim', opts = {},
 	config = function()
-		-- document existing key chains
-		require('which-key').register {
-			['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-			['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-			['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-			['<leader>gh'] = { name = '[G]it [H]unk', _ = 'which_key_ignore' },
-			['<leader>h'] = { name = '[H]arpoon'},
-			['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-			['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-			['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-		}
-		-- register which-key VISUAL mode
-		require('which-key').register({
-			['<leader>'] = { name = 'VISUAL <leader>' },
-			['<leader>h'] = { 'Git [H]unk' },
-		}, { mode = 'v' })
+		require('which-key').add({
+			-- Command Mode Keybindings
+			{'<leader>c', desc = '[C]ode', _ = 'which_key_ignore' },
+			{'<leader>d', desc = '[D]ocument', _ = 'which_key_ignore' },
+			{'<leader>g', desc = '[G]it', _ = 'which_key_ignore' },
+			{'<leader>gh', desc = '[G]it [H]unk', _ = 'which_key_ignore' },
+			{'<leader>h', desc = '[H]arpoon'},
+			{'<leader>r', desc = '[R]ename', _ = 'which_key_ignore' },
+			{'<leader>s', desc = '[S]earch', _ = 'which_key_ignore' },
+			{'<leader>w', desc = '[W]orkspace', _ = 'which_key_ignore' },
+
+			-- Visual Mode Keybindings
+			{'<leader>gh', desc = 'Git [H]unk', mode = 'v' },
+		})
 	end
 }
