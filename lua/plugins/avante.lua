e806298320b30a94e4c30eff28ea3904a7fafee4
Nick Burt <nbur4556@gmail.com>
2024-12-28 19:06:31 -0600

    include ollama as a vendor for avante plugin

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,7 +5,17 @@ return {
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
-    -- add any opts here
+    provider = "ollama",
+    vendors = {
+      ollama = {
+        __inherited_from = "openai",
+        endpoint = "http://10.0.0.91:11434",
+        api_keyname = "",
+        model = "deepseek-coder-v2",
+        temperature = 0,
+        max_tokens = 4096
+      },
+    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
@@ -15,9 +25,9 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
-    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-    "zbirenbaum/copilot.lua", -- for providers='copilot'
+    "zbirenbaum/copilot.lua",      -- for providers='copilot'
     {
       -- support for image pasting
       "HakonHarnes/img-clip.nvim",
