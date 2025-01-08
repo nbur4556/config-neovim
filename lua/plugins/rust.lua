6805b6b35c01f1e7c55497dbeadb7ffdcdefc26b
Nick Burt <nbur4556@gmail.com>
2025-01-08 12:12:19 -0600

    reinclude lualine, ministarter, rust, and vimsurround

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,8 +1,8 @@
--- -- rust language integrations
--- return {
--- 	'rust-lang/rust.vim',
--- 	ft = "rust",
--- 	init = function ()
--- 		vim.g.rustfmt_autosave = 1
--- 	end
--- };
+-- rust language integrations
+return {
+	'rust-lang/rust.vim',
+	ft = "rust",
+	init = function ()
+		vim.g.rustfmt_autosave = 1
+	end
+};
