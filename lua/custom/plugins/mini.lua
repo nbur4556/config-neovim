c85222efba818165d85c71b6138e4e67da66f23f
Nick Burt <nbur4556@gmail.com>
2026-07-01 16:14:07 -0500

    shorten the git graph decorate

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -3,7 +3,7 @@ require('mini.git').setup()
 vim.keymap.set('n', '<leader>ga', '<cmd>Git add -A<CR>', { desc = 'Git add all' })
 vim.keymap.set('n', '<leader>gc', '<cmd>Git commit<CR>', { desc = 'Git commit' })
 vim.keymap.set('n', '<leader>gs', '<cmd>Git status<CR>', { desc = 'Git status' })
-vim.keymap.set('n', '<leader>gg', '<cmd>Git log --graph --oneline --all<CR>', { desc = 'Git graph' })
+vim.keymap.set('n', '<leader>gg', '<cmd>Git log --graph --oneline --all --decorate=short<CR>', { desc = 'Git graph' })
 
 require('mini.move').setup()
 -- usage: Alt = directional keys should move the line in normal mode
